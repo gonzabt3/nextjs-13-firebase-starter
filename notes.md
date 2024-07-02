84c914a47292d708bd4928d6016236e10e526622
gonza <gonzalo.muscolo@gmail.com>
2024-07-02 17:53:12 -0300

    adding customer component

--- notes.md
+++ notes.md
@@ -32,5 +32,10 @@ menus
     }
   ]
 
+qr code---
+create menus
+create qr code using cloud function, url -> menu/Id
+    needs to create a new page and extact all the component from [restaurantName]/page 
+store the qr in firestore or something like that.
 
 
--- src/app/menu/[restaurantId]/page.tsx
+++ src/app/menu/[restaurantId]/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
+import { collection,doc, getDoc, getFirestore, query, where } from "firebase/firestore";
 import firebase_app from "@/firebase/config";
-import CustomerMenu from "../components/CustomerMenu";
+import CustomerMenu from "../../components/CustomerMenu";
 
-
-const replaceSpaces = (string: string) => string.replace(/%20/g, ' ');
 export default function Menu() {
-  const restaurantName : any = useParams().restaurantName;
+  const restaurantId : any = useParams().restaurantId;
   const [menu, setMenu] = useState<any>(null)
   const [showErrorNotFound, setShowErrorNotFound] = useState(false);
   const  [loading, setLoading] = useState(true);
@@ -19,15 +17,12 @@ export default function Menu() {
     const fetchRestaurant = async () => {
       try {
         const db = getFirestore(firebase_app);
-        const queryRestaurantName = replaceSpaces(restaurantName);
-
-        console.log(queryRestaurantName);
-        const q = query(collection(db, "menus"), where("restaurantName", "==", queryRestaurantName));
-        const querySnapshot = await getDocs(q);
-
-        if (!querySnapshot.empty) {
+        const docRef = doc(db, 'menus', restaurantId)
+        const docSnap = await getDoc(docRef)
+        
+        if (docSnap.exists()) {
           // Assuming there's only one restaurant with the given name
-          const restaurantData :any = querySnapshot.docs[0].data();
+          const restaurantData :any = docSnap.data();
           console.log("Restaurant data:", restaurantData);
           setMenu(restaurantData)
           setLoading(false);
@@ -51,4 +46,3 @@ export default function Menu() {
     <CustomerMenu menu={menu} loading={loading} showErrorNotFound={showErrorNotFound} />  
   );
 }
-
